e27e2bad089fc0fc92b16c68fca3ef82e17d3c15
Riley Anderson <101540525+rileyandrsn@users.noreply.github.com>
2025-07-23 21:35:23 -0700

    chore: update index.tsx

--- portfolio/src/Pages/Home.tsx
+++ portfolio/src/Pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './Resources/home.css';
 import portfolioImage from './Resources/portfolio.png';
 
 function App() {
   return (
 
     <aside>
-    <img src={portfolioImage} width={300} height={400}/>
+    <img src={portfolioImage} width={300} height={350}/>
     <h3>Riley Anderson</h3>
     <h4>Computer science student at Arizona State University</h4>
     <ul>
--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
