4ed8fb90c06029be2aab5c9fdb32f8fb97bc29ef
Riley Anderson <101540525+rileyandrsn@users.noreply.github.com>
2025-07-23 21:30:45 -0700

    chore: aside element

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,20 +4,20 @@ import portfolioImage from './Resources/portfolio.png';
 
 function App() {
   return (
-    <main>
-      <article>
-        <img src={portfolioImage} width={300} height={300}/>
-        <h3>Riley Anderson</h3>
-        <h4>Computer science student at Arizona State University</h4>
-        <ul>
-          <li>Puyallup, WA and Tempe, AZ</li>
-          <li>Sophomore</li>
-          <li>Email</li>
-          <li>LinkedIn</li>
-          <li>Github</li>
-        </ul>
-      </article>
-    </main>
+
+    <aside>
+    <img src={portfolioImage} width={300} height={400}/>
+    <h3>Riley Anderson</h3>
+    <h4>Computer science student at Arizona State University</h4>
+    <ul>
+      <li>Puyallup, WA and Tempe, AZ</li>
+      <li>Sophomore</li>
+      <li>Email</li>
+      <li>LinkedIn</li>
+      <li>Github</li>
+    </ul>
+  </aside>
+
   );
 }
 
