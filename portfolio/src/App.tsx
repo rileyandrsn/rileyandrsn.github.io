ef534c1f45f78083fbfaf54f709d4e8eecfcbf74
Riley Anderson <101540525+rileyandrsn@users.noreply.github.com>
2025-07-23 21:16:28 -0700

    chore: add portfolio article

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import portfolioImage from './Resources/portfolio.png';
 
 function App() {
   return (
-    <main>Riley Anderson</main>
+    <main>
+      <article>
+        <img src={portfolioImage} width={300} height={300}/>
+        <h3>Riley Anderson</h3>
+        <h4>Computer science student at Arizona State University</h4>
+        <ul>
+          <li>Puyallup, WA and Tempe, AZ</li>
+          <li>Sophomore</li>
+          <li>Email</li>
+          <li>LinkedIn</li>
+          <li>Github</li>
+        </ul>
+      </article>
+    </main>
   );
 }
 
